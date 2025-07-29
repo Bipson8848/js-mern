b209158ff009e33a81e54210c7a7deec81f70f7c
unknown <bipson490@gmail.com>
2025-07-29 07:56:24 +0545

    jwa

--- derived.js
+++ derived.js
@@ -11,5 +11,24 @@ let users=[
     ["Judy", 26, "Marketer"]
 ]
 users.forEach(user => {
-    console.log(user[0], user[1], user[2]);
+    // console.log(user[0], user[1], user[2]);
 })
+let obj={
+    name: "John",
+    age: 30,
+    occupation: "Developer"
+}
+// console.log(obj.name,obj.age,obj.occupation);
+
+let arrobj=[
+    {name: "Alice", age: 25, occupation: "Engineer"},
+    {name: "Bob", age: 30, occupation: "Designer"},
+    {name: "Charlie", age: 35, occupation: "Teacher"},
+    {name: "Diana", age: 28, occupation: "Artist"},
+    {name: "Ethan", age: 22, occupation: "Developer"},
+    {name: "Bipson", age:17, occupation: "Student"}
+]
+arrobj.forEach((e)=>{
+    console.log(`Hello ${e.name}`);
+})
+
