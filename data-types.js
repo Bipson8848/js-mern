4c579c60adacc72ef15b450e18e3e1fb73257a57
unknown <bipson490@gmail.com>
2025-07-29 06:44:23 +0545

    ha

---
+++ data-types.js
@@ -0,0 +1,7 @@
+let a=true
+let b=false
+let c=null
+let d=undefined
+let e=10n
+console.log(a, b, c, d, e);
+console.log(typeof a, typeof b, typeof c, typeof d, typeof e);
--- syntax.js
+++ syntax.js
@@ -1,1 +1,7 @@
-console.log('hello World');
+const a=10
+console.log(a);
+{
+    const a=20;
+    console.log(a);
+}
+console.log(a);
