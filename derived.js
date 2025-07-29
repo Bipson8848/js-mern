21abd598e8edafd3810b9303b2cb6bfc02bbf695
unknown <bipson490@gmail.com>
2025-07-29 07:11:54 +0545

    haod

--- derived.js
+++ derived.js
@@ -1,2 +1,15 @@
-let array=["user", "admin", "guest"];
-console.log(array[0], array[1], array[2]);
+let users=[
+    ["Alice", 25, "Engineer"],
+    ["Bob", 30, "Designer"],
+    ["Charlie", 35, "Teacher"],
+    ["Diana", 28, "Artist"],
+    ["Ethan", 22, "Developer"],
+    ["Fiona", 27, "Manager"],
+    ["George", 32, "Analyst"],
+    ["Hannah", 29, "Consultant"],
+    ["Ian", 31, "Salesperson"],
+    ["Judy", 26, "Marketer"]
+]
+users.forEach(user => {
+    console.log(user[0], user[1], user[2]);
+})
