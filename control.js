8acc64838db6838e2c5e920120e5a46ec3b738c0
unknown <bipson490@gmail.com>
2025-08-06 07:16:25 +0545

    mern

---
+++ control.js
@@ -0,0 +1,10 @@
+let day="jas"
+if(day==='sunday'|| day==="saturday"){
+    console.log("It's a holiday!");
+}else{
+    if(day==='friday'){
+        console.log("It's weekend!");
+    }else {
+        console.log("It's a working day.");
+    }
+}
--- operator.js
+++ operator.js
@@ -1,5 +1,29 @@
-let a=10;
-console.log(++a);
-console.log(a++);
-console.log(--a);
-console.log(a--);
+let abc;
+// console.log(abc); // undefined
+if (!abc) {
+    // console.log("abc is not defined or is falsy");
+}
+let userName= "John";
+let notificationCount = 10;
+let emailTemplate =`Hello ${userName}, you have ${notificationCount} new notifications. Please check your inbox.`;
+// console.log(emailTemplate);
+
+//struture and destructuring
+let person=['Bipson','Shrestha',18]
+let [name, surname, age] = person;
+console.log(name, surname, age); 
+
+let user={
+    uName: 'Bipson',
+    uSurname: 'Shrestha',
+    uAge: 18,
+    uEmail: 'bipson490@gmail.com',
+    uPhone: '9847620049'
+}
+let newUser=user
+console.log(user.uName); // Both will show 'Bipson Shrestha'
+newUser.uName='Bipson Shrestha';
+console.log(user.uName); // Both will show 'Bipson Shrestha'
+
+// let {uName,uPhone}=user
+// console.log(`userName:${uName}, phone:${uPhone}`)
